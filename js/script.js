2976dcd36df03f4ab16cfa32712c848bb8827c6d
stephanytt <stephany.tesouza@gmail.com>
2017-05-15 17:58:13 +0000

    Inserindo pagina de orcamento

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(main);
  
 function main(){
+    
     $(".menu li").click(function(){
        var pagina = $(this).html();
        var url="";
@@ -24,7 +25,29 @@ function main(){
         $.get(url,function(data){
             $("main").html(data);
         })
-    }   
-    
-    );
+    });
+}
+
+function clica(x){
+       var altimg = $(x).attr("alt");
+       var img="";
+       var titulo="";
+       switch (altimg) {
+           case 'caricaturas':
+                img = "imgs/noivinhos.png";
+                titulo="Caricaturas";
+               break;
+            case 'miniaturas':
+                img= "imgs/miniatura.png";
+                titulo= "Miniaturas";
+               break;
+            case 'noivinhos':
+                img = "imgs/noivinhos.png";
+                titulo= "Noivinhos";
+               break;
+       }
+        $.get("/orcamento.html",function(data){
+            var form = document.querySelector("#teste");
+            form.innerHTML = data;
+        });
 }
