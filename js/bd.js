e63ce7dec08971ead29e19484021d33a8c248a56
stephanytt <stephany.tesouza@gmail.com>
2017-05-25 12:13:14 +0000

    Bd-Inserir pedido/Buscar

--- js/bd.js
+++ js/bd.js
@@ -3,3 +3,53 @@ function inserirnoCarrinho(){
     var qtd = parseInt(document.forms.form1.qtd.value);
     inserir({"name":name,"qtd":qtd});
 }
+
+//{id: 12345, name: "miniaturas", qtd: 2}
+function inserir(pedido){
+    var open = indexedDB.open("Bd-pedido", 1);
+    open.onupgradeneeded = function() {
+        var db = open.result;
+        var store = db.createObjectStore("MyObjectStore", {keyPath: "name"});
+        //store.createIndex("NameIndex", ["name.first"]);
+    };
+    
+    open.onsuccess = function() {
+        var db = open.result;
+        var tx = db.transaction("MyObjectStore", "readwrite");
+        var store = tx.objectStore("MyObjectStore");
+        //var index = store.index("NameIndex");
+    
+        store.put(pedido);
+        
+        tx.oncomplete = function() {
+            alert("Inserido com sucesso")
+            db.close();
+        };
+    }
+    
+}
+
+function buscar(){
+    getObjId(parseInt(document.forms.form2.id.value));
+}
+
+function getObjId(id){
+    var open = indexedDB.open("Bd-pedido", 1);
+    open.onsuccess = function(){
+        var db = open.result;
+        var tx = db.transaction("MyObjectStore", "readwrite");
+        var store = tx.objectStore("MyObjectStore");
+        
+        var getFirst = store.get(id);
+        
+        getFirst.onsuccess = function() {
+            var pedido = getFirst.result;
+           document.getElementById("h1").innerHTML = pedido.name + " " + pedido.qtd; 
+        };
+        
+        tx.oncomplete = function() {
+            db.close();
+        };
+    }
+}
+
